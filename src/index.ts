5209401e0543f269059a478f25f21025c695a257
jjrv <juha@busfaster.com>
2017-09-26 17:56:01 +0300

    Export SRS class.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // This file is part of charto-model, copyright (c) 2017 BusFaster Ltd.
 // Released under the MIT license, see LICENSE.
 
+export { SRS } from './SRS';
 export { BBox } from './BBox';
 export { Feature } from './Feature';
 
