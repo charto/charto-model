e87c71966c86c43b1bf94a33f7af6fbfbc28fe99
jjrv <juha@busfaster.com>
2017-09-26 17:55:45 +0300

    Remove bluebird dependency.

--- src/layer/GroupLayer.ts
+++ src/layer/GroupLayer.ts
@@ -1,8 +1,6 @@
 // This file is part of charto-model, copyright (c) 2017 BusFaster Ltd.
 // Released under the MIT license, see LICENSE.
 
-import * as Promise from 'bluebird';
-
 import { Layer, LayerFeatures } from './Layer';
 import { Feature } from '../Feature';
 import { BBox } from '../BBox';
@@ -14,9 +12,10 @@ export class GroupLayer extends Layer {
 	}
 
 	getLayerFeatures(bbox: BBox) {
-		const ready: Promise<LayerFeatures> = Promise.map(
-			this.layerList,
-			(layer: Layer) => layer.getLayerFeatures(bbox)
+		const ready: Promise<LayerFeatures> = Promise.all(
+			this.layerList.map(
+				(layer: Layer) => layer.getLayerFeatures(bbox)
+			)
 		).then((lists: LayerFeatures[]) => Array.prototype.concat.apply([], lists));
 
 		return(ready);
--- src/layer/Layer.ts
+++ src/layer/Layer.ts
@@ -1,8 +1,6 @@
 // This file is part of charto-model, copyright (c) 2017 BusFaster Ltd.
 // Released under the MIT license, see LICENSE.
 
-import * as Promise from 'bluebird';
-
 import { Feature } from '../Feature';
 import { BBox } from '../BBox';
 
